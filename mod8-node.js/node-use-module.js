a158bc977cb7c1fecfd5cc293ca9f60dec6b8c81
Artem Antonian <art902004@gmail.com>
2025-07-04 20:17:20 +0300

    Add files via upload

---
+++ mod8-node.js/node-use-module.js
@@ -0,0 +1,4 @@
+/* Импортируйте функцию из модуля mymodule.js. Вызовите функцию и результат выведите в console.log(). Запустите текущий скрипт в Node.js: node node-use-module.js и убедитесь, что результат показан верно */
+import isOdd from 'is-odd'
+console.log(isOdd(21))
+console.log(isOdd(42))
